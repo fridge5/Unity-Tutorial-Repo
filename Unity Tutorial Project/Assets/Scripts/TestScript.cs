8b15fa4820553fa661776fe36075eb3a36521b11
fridge5 <1736721@fife.ac.uk>
2023-09-27 12:49:38 +0100

    Added various variables to my unity project to test string variables

--- Unity Tutorial Project/Assets/Scripts/TestScript.cs
+++ Unity Tutorial Project/Assets/Scripts/TestScript.cs
@@ -3,24 +3,48 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class TestScript : MonoBehaviour
+    
 {
+    string combinedString;
+
+    [SerializeField]
+    string editorString = "Hello";
     // Start is called before the first frame update
     void Start()
-    {
+
+    
+    {   /*
         Debug.Log("YOU WERE THE CHOSEN ONE!");
 
         Debug.Log("IT WAS SAID YOU WOULD DESTROY THE SITH, NOT JOIN THEM!");
 
         Debug.Log("BRING BALANCE TO THE FORCE, NOT LEAVE IT IN DARKNESS");
+        */
 
-        transform.Translate(0, 0, 0);
+        string firstString;
+
+        firstString = "You shall not";
+        string secondString = " PASS!";
+        combinedString = firstString + secondString;
+
+        Debug.Log(combinedString);
+
+        Debug.Log(editorString);
+
+       
+
+
+
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log("YOU WERE MY BROTHER ANAKIN");
+        //Debug.Log("YOU WERE MY BROTHER ANAKIN");
 
-        transform.Translate(1, 0, 0);
+        //transform.Translate(0.5f, 0, 0);
+
+        Debug.Log(combinedString); 
     }
 }
