e0626e67fe0971690b512655bc84fa14aed487bc
fridge5 <1736721@fife.ac.uk>
2023-11-01 11:27:32 +0000

    Added new variable for target scaling for it to scale by 1 each time and also added player sprite and sprite color into my unity project

--- Unity Tutorial Project/Assets/Scripts/TestScript2.cs
+++ Unity Tutorial Project/Assets/Scripts/TestScript2.cs
@@ -3,23 +3,49 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class TestScript2 : MonoBehaviour
+
 {
+    [SerializeField]
+    string editorString = "Hello There";
+   
+    [SerializeField]
+    float targetScale = 0;
+
+    Vector3 changingScale = new Vector3(1.0f, 1.0f, 1.0f);
+
+    [SerializeField]
+    SpriteRenderer playerSprite = null;
+
+    [SerializeField]
+    Color spriteColor = Color.white;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("You have my sword.");
-        Debug.LogWarning("And my bow.");
-        Debug.LogError("And my axe!");
+      //Debug.Log("You have my sword.");
+      //Debug.LogWarning("And my bow.");
+      //Debug.LogError("And my axe!");
+
+      playerSprite.color = spriteColor;
+
 
     //Moves and Rotates object on screen via x, y, z coordinates.
-        transform.Translate(2, 0, 0);
+      //transform.Translate(2, 0, 0);
 
-        transform.Rotate(0, 0, 45);
+      //transform.Rotate(0, 0, 45);
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(0, 0, 15);
+        targetScale = targetScale + 0.01f;
+   //transform.Rotate(0, 0, 15);
+
+     //changingScale.x = targetScale;
+     // changingScale.y = targetScale;
+     // changingScale.z = targetScale;
+
+        transform.localScale = new Vector3(targetScale, targetScale, targetScale);
     }
 }
